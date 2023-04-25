93a64478280d3a8043809418e564c5265ce3df0c
Eddy Mendoza <eddy.mendoza@osf.pe>
2023-04-25 10:31:47 -0500

    se importa getvalue y setvalue para usar remember y dejar de usar value, ahora debe ser var

--- app/src/main/java/com/eddymy1304/estadosprueba2/MainActivity.kt
+++ app/src/main/java/com/eddymy1304/estadosprueba2/MainActivity.kt
@@ -11,9 +11,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,17 +44,17 @@ fun MyStateExample() {
     // para guardar las variables
     // remember
     //remembersaveable
-    val counter = rememberSaveable { mutableStateOf(0) }
+    var counter by rememberSaveable { mutableStateOf(0) }
 
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { counter.value += 1 }) {
+        Button(onClick = { counter += 1 }) {
             Text(text = "Pulsar")
         }
-        Text(text = "El boton ha sido pulsado ${counter.value} veces")
+        Text(text = "El boton ha sido pulsado $counter veces")
     }
 }
 
